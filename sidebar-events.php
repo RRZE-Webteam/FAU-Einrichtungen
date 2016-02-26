4e4f7e9fccd1e28e45df5ab5b31c2f49d17269b1
Wolfgang Wiese <xwolf@xwolf.de>
2016-02-26 16:49:15 +0100

    Datum bei Top event anzeigen und Kalender-CSS-im Backend verbessert

--- sidebar-events.php
+++ sidebar-events.php
@@ -9,19 +9,19 @@
 global $options;
 $topevent_posts = get_posts(array('tag' => $options['start_topevents_tag'], 'numberposts' => $options['start_topevents_max']));
 foreach($topevent_posts as $topevent): ?>
-    <div class="widget">
+    <div class="widget h-event vevent">
 	    <?php 
 	    $titel = get_post_meta( $topevent->ID, 'topevent_title', true );
 	    if (strlen(trim($titel))<3) {
 		$titel =  get_the_title($topevent->ID);
 	    } 
-	    $link = get_permalink($topevent->ID);
+	    $link = fau_esc_url(get_permalink($topevent->ID));
 
 	    ?>
-	    <h2 class="small"><a href="<?php echo $link; ?>"><?php echo $titel; ?></a></h2>
+	    <h2 class="small p-name"><a class="url u-url" href="<?php echo $link; ?>"><?php echo $titel; ?></a></h2>
 	    <div class="row">
 		<?php 
-
+		    
 		    $imageid = get_post_meta( $topevent->ID, 'topevent_image', true );
 		    $imagehtml = '';
 		    if (isset($imageid) && ($imageid>0)) {
@@ -45,11 +45,18 @@ foreach($topevent_posts as $topevent): ?>
 		<?php } else { ?>
 		    <div class="span4">
 		<?php } 
+			$topevent_date  = get_post_meta( $topevent->ID, 'topevent_date', true );
+			if (!empty($topevent_date)) {
+			    echo '<div class="topevent_date dtstart dt-start" title="'.$topevent_date.'">';
+			    echo date_i18n( get_option( 'date_format' ), strtotime( $topevent_date ) );
+			    echo "</div>\n";
+			}
+			
 			$desc = get_post_meta( $topevent->ID, 'topevent_description', true );
 			if (strlen(trim($desc))<3) {
 			    $desc =  fau_custom_excerpt($topevent->ID,$options['default_topevent_excerpt_length']);
 			}  ?>   
-			<div class="topevent-description"><?php echo $desc; ?></div>
+			<div class="topevent-description summary p-summary"><?php echo $desc; ?></div>
 
 		    </div>			
 	    </div>
