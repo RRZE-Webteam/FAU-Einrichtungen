5c29a0f2c56bb0d4c4873e3a7e2475b94647f723
Wolfgang Wiese <xwolf@xwolf.de>
2016-04-18 11:19:36 +0200

    Bugfix jQuery-Anweisung https://github.com/RRZE-Webteam/FAU-Einrichtungen/issues/43 Sprachdateiupdate

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
 	
 	
 	// Smooth scrolling for anchor-links (excluding accordion-toggles)
-	$('a[href*=#]:not([href=#]):not(.accordion-toggle):not(.accordion-tabs-nav-toggle)').click(function() {
+	$('a[href*="#"]:not([href="#"]):not(.accordion-toggle):not(.accordion-tabs-nav-toggle)').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
