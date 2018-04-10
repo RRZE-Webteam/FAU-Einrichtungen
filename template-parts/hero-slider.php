168cfd4a3f682dd99920dd77be822709aa75282e
bmehler <bernhard.mehler@gmail.com>
2018-04-10 12:52:57 +0200

    - Carousel Slider soweit fertig - Next/Previous Buttons müssen noch ausgerichtet werden

--- template-parts/hero-slider.php
+++ template-parts/hero-slider.php
@@ -16,6 +16,7 @@
     <?php	
     global $usejslibs;
     global $defaultoptions;
+    $i= 0;
 
     $numberposts = get_theme_mod('start_header_count');
     $catid =  get_theme_mod('slider-catid');
@@ -32,8 +33,17 @@
 	);                   
 	$hero_posts = get_posts($query); 
     }
+    ?>
+    <div class="bs-example">
+       <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner"><?php
     foreach($hero_posts as $hero): ?>
-	<!--<div class="hero-slide">
+	<!--<div class="hero-slide">-->
+		 <?php if($i == 0) { ?>
+                <div class="item active">
+                <?php $i++;} else { ?>
+                        <div class="item">  
+                <?php } ?>
 		<?php 
 
 		$sliderimage = '';
@@ -95,7 +105,7 @@
 		    echo '<p class="credits">'.$copyright."</p>";
 		}
 		?>
-		<!--<div class="hero-slide-text">
+		<div class="hero-slide-text">
 		    <div class="container">
 			<div class="row">
 			    <div class="slider-titel">
@@ -128,12 +138,13 @@
 			    </div>  <?php } ?>		   
 		    </div>
 		</div>
-	    </div> -->
+	    </div>
     <?php endforeach; 
       wp_reset_query();
+      ?></div></div></div>
     ?>
                 
-    <div class="bs-example">
+    <!--<div class="bs-example">
        <div id="myCarousel" class="carousel slide" data-ride="carousel">
            <!-- Carousel indicators -->
            <!--<ol class="carousel-indicators">
@@ -142,7 +153,7 @@
                <li data-target="#myCarousel" data-slide-to="2"></li>
            </ol>   
            <!-- Wrapper for carousel items -->
-           <div class="carousel-inner">
+           <!--<div class="carousel-inner">
                <div class="item active">
                    <img src="http://lorempixel.com/1260/350/abstract/3" alt="First Slide">
                     <div class="carousel-caption">
@@ -164,8 +175,8 @@
                         <p>Beautiful flowers in Kolymbari, Crete.</p>
                     </div>
                </div>
-           </div>
-           <!-- Carousel controls -->
+           </div>-->
+           <!--Carousel controls -->
             <button class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
                 <i class="fa fa-arrow-circle-left fa-3x" aria-hidden="true"></i>
             </button>
@@ -173,15 +184,15 @@
                 <i class="fa fa-arrow-circle-right fa-3x" aria-hidden="true"></i>
             </button>
                <div id="carouselButtons">
-                <button id="playButton" type="button" class="btn btn-default btn-xs">
+                <button id="playButton" type="button" class="btn btn-default">
                     <i class="fa fa-play" aria-hidden="true"></i>
                  </button>
-                <button id="pauseButton" type="button" class="btn btn-default btn-xs">
+                <button id="pauseButton" type="button" class="btn btn-default">
                     <i class="fa fa-pause" aria-hidden="true"></i>
                 </button>
             </div>
-        </div>
-    </div>       
+       <!-- </div>
+    </div> -->      
                 
                 
     <script type="text/javascript">
@@ -201,20 +212,106 @@
     </script>
 
     <style>
-           #carouselButtons {
+        
+        i.fa.fa-play, i.fa.fa-pause {
+    color: rgba(0, 51, 102, 1);
+    opacity:1;
+}
+button#playButton, button#pauseButton {
+          opacity: 0.70;
+    border-radius: 0;
+    background-color: #fff;
+}
+
+button#playButton:hover, button#pauseButton:hover {
+       opacity: 1;
+    border-radius: 0;
+    background: #fff;
+}
+.
+        
+   .carousel-inner>.item>img, .carousel-inner>.item>a>img {
+    display: block;
+    max-width: none;
+    height: auto;
+    line-height: 1;
+}
+        
+    #hero-slides .hero-slide-text {
+        top: initial;
+
+    }
+  #carouselButtons {
     right: 15%;
     top: 86%;
     position: absolute;
     bottom: 0px;
+    z-index: 100;
 }
     
     .carousel-control.left, .carousel-control.right {
   border:none;
-    background-image: none;}
+    background-image: none;
+    z-index: 100;}
+    
+    /*
+inspired from https://codepen.io/Rowno/pen/Afykb 
+*/
+.carousel-fade .carousel-inner .item {
+  opacity: 0;
+  transition-property: opacity;
+}
+
+.carousel-fade .carousel-inner .active {
+  opacity: 1;
+}
+
+.carousel-fade .carousel-inner .active.left,
+.carousel-fade .carousel-inner .active.right {
+  left: 0;
+  opacity: 0;
+  z-index: 1;
+}
+
+.carousel-fade .carousel-inner .next.left,
+.carousel-fade .carousel-inner .prev.right {
+  opacity: 1;
+}
+
+.carousel-fade .carousel-control {
+  z-index: 2;
+}
+
+/*
+WHAT IS NEW IN 3.3: "Added transforms to improve carousel performance in modern browsers."
+now override the 3.3 new styles for modern browsers & apply opacity
+*/
+@media all and (transform-3d), (-webkit-transform-3d) {
+    .carousel-fade .carousel-inner > .item.next,
+    .carousel-fade .carousel-inner > .item.active.right {
+      opacity: 0;
+      -webkit-transform: translate3d(0, 0, 0);
+              transform: translate3d(0, 0, 0);
+    }
+    .carousel-fade .carousel-inner > .item.prev,
+    .carousel-fade .carousel-inner > .item.active.left {
+      opacity: 0;
+      -webkit-transform: translate3d(0, 0, 0);
+              transform: translate3d(0, 0, 0);
+    }
+    .carousel-fade .carousel-inner > .item.next.left,
+    .carousel-fade .carousel-inner > .item.prev.right,
+    .carousel-fade .carousel-inner > .item.active {
+      opacity: 1;
+      -webkit-transform: translate3d(0, 0, 0);
+              transform: translate3d(0, 0, 0);
+    }
+}
 
     </style>
           
             
 </div>
+
     <?php get_template_part('template-parts/hero', 'jumplinks'); ?>	
 </section>
