f9efb56e8a47da2af206709284accfa54a26501f
Wolfgang Wiese <xwolf@xwolf.de>
2018-09-25 14:36:24 +0200

    Update search-header.php
    id aus submit rausgenommen

--- template-parts/search-header.php
+++ template-parts/search-header.php
@@ -34,7 +34,7 @@ if (is_plugin_active('rrze-search/rrze-search.php')) {
             }
         }
         ?>
-        <input type="submit" id="searchsubmit" value="<?php _e('Finden','fau'); ?>">
+        <input type="submit" value="<?php _e('Finden','fau'); ?>">
     </form>
 <?php }
 echo PHP_EOL;
