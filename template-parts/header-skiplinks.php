e7502f780528513e117017bfd9e3ba9367a43a15
Wolfgang Wiese <xwolf@xwolf.de>
2021-04-26 10:04:41 +0200

    - Rechtschreibungs-Issue: Hauptmenu wird zu Navigation

--- template-parts/header-skiplinks.php
+++ template-parts/header-skiplinks.php
@@ -11,7 +11,7 @@
 <nav id="skiplinks" aria-label="<?php _e('Sprunglinks','fau'); ?>">
     <ul class="jumplinks">
 	<li><a href="#content"><?php _e('Zum Inhalt springen','fau'); ?></a></li>
-	<li><a href="#nav"><?php _e('Zum Hauptmenu springen','fau'); ?></a></li>
+	<li><a href="#nav"><?php _e('Zur Navigation springen','fau'); ?></a></li>
 	<li><a href="#footer"><?php _e('Zum Seitenende springen','fau'); ?></a></li>
     </ul>
 </nav>
