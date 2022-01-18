50c314341529e550f11ab3c5c656c10ab6192dfa
Wolfgang Wiese <xwolf@xwolf.de>
2022-01-18 15:05:42 +0100

    FAU Form raus, da das dort nicht mehr so benutzt wird

--- template-parts/hero-sliderpage-jumplinks.php
+++ template-parts/hero-sliderpage-jumplinks.php
@@ -13,21 +13,7 @@ if (('' != get_theme_mod( 'advanced_activate_quicklinks' )) && (true== get_theme
     <nav class="hero-navigation" aria-label="<?php echo __('Quicklinks','fau'); ?>">
 	<div class="container">
 	    <div class="row">
-		    <?php 
-		    if (get_theme_mod('website_type')==-1) { ?>
-		     <div class="quicklinks">
-			    <?php if(has_nav_menu('quicklinks-1')) { ?>
-				    <p class="headline"><?php echo fau_get_menu_name('quicklinks-1'); ?></p>
-				    <?php wp_nav_menu( array( 'theme_location' => 'quicklinks-1', 'container' => false, 'items_wrap' => '<ul class="%2$s">%3$s</ul>' ) ); 
-			    } ?>
-		    </div>
-		    <div class="quicklinks">
-			    <?php if(has_nav_menu('quicklinks-2')) { ?>
-				    <p class="headline"><?php echo fau_get_menu_name('quicklinks-2'); ?></p>
-				    <?php wp_nav_menu( array( 'theme_location' => 'quicklinks-2', 'container' => false, 'items_wrap' => '<ul class="%2$s">%3$s</ul>' ) );  
-			    } ?>
-		    </div>
-		    <?php } else { ?>
+
 		    <div role="presentation" class="infobar">				    
 			<?php 
 		       $header_image = get_header_image();
@@ -39,7 +25,7 @@ if (('' != get_theme_mod( 'advanced_activate_quicklinks' )) && (true== get_theme
 			}
 			?>
 		    </div>
-		    <?php } ?>
+
 		    <div class="quicklinks">
 			    <?php if(has_nav_menu('quicklinks-3')) { ?>
 				    <p class="headline"><?php echo fau_get_menu_name('quicklinks-3'); ?></p>
