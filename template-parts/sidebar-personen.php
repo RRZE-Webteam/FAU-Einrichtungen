1ae40695cbd638100b3b25fcb920b959f92298e7
Wolfgang Wiese <xwolf@xwolf.de>
2022-01-20 17:23:14 +0100

    #965

--- template-parts/sidebar-personen.php
+++ template-parts/sidebar-personen.php
@@ -1,18 +1,22 @@
 <?php 
 
-
-    if (class_exists( 'FAUPersonWidget' ) )  {
+if ( is_plugin_active( 'fau-person/fau-person.php' ) ) { 
+   // if (class_exists( 'FAUPersonWidget' ) )  {
 	$sidebar_personen = get_post_meta( $post->ID, 'sidebar_personen', true );
 	$sidebar_title_personen = get_post_meta( $post->ID, 'sidebar_title_personen', true );	
 	if(isset($sidebar_personen) && !empty($sidebar_personen))  { 
 	   $persons = $sidebar_personen;
            fau_use_sidebar(true);
-
+	    $hstart = 2;
 	   if (!fau_empty($sidebar_title_personen)) {
 		echo '<h2 class="widget-title">'.$sidebar_title_personen.'</h2>';
+		$hstart= 3;
 	   }
+	   $params = '';
 	   foreach($persons as $person) {
-		the_widget('FAUPersonWidget', array('id' => $person)); 
+	       $params = 'id="'.$person.'" format="sidebar" hstart="'.$hstart.'"';	       
+	       echo do_shortcode('[kontakt '.$params.']'); 
+
 	   } 
        } 
     }
