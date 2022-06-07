3997ebb2b23df9690efc3d3f3abd84461f469230
Wolfgang Wiese <xwolf@xwolf.de>
2022-06-07 16:47:11 +0200

    Übernahme Updates zu Print Versionsnummernupdate

--- functions/constants.php
+++ functions/constants.php
@@ -22,7 +22,7 @@ $defaultoptions = [
 		// Ansonsten kann er manuell über 'gulp nodebug' auf false und
 		//  mit 'gulp debugmode' auf true gesetzt werden
 		// Oder hier von Hand :)
-    'js-version'				=> '2.01',
+    'js-version'				=> '2.10',
 		// Theme-Versionslinie, wird überschrieben durch Style.css Version
     
     'website_type'			=> 1,
