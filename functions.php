8adfe7128282e1de87b93607a9f29441b0244d56
Wolfgang Wiese <xwolf@xwolf.de>
2022-07-15 09:20:42 +0200

    #1182

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ function fau_setup() {
         }
 
 
-	add_theme_support('html5', array( 'comment-list', 'comment-form', 'search-form' ));
+	add_theme_support('html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ));
 	add_theme_support('caption');
 	add_theme_support('title-tag');
 	add_theme_support('automatic-feed-links');
