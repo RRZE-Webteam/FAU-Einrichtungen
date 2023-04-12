2a7b578a46453cbc986257b4732e91b3b606dbc4
Marten Manthey <martenmanthey1995@gmail.com>
2023-04-12 17:55:36 +0200

    #1226: Standard-Template + Bildlinks Fix

--- page.php
+++ page.php
@@ -45,8 +45,7 @@ while ( have_posts() ) :
 					    ?>
 					</div>    
 					
-			    </main>
-			    <?php  
+					<?php  
 			    $logoliste = get_post_meta( $post->ID, 'fauval_imagelink_catid', true );		
 			    if ($logoliste) { 
 				    /* New since 1.10.57 */
@@ -54,11 +53,12 @@ while ( have_posts() ) :
 				$size = $logosize != '' ? esc_attr($logosize) : "logo-thumb";
 				$logos = fau_imagelink_get(array('size' => $size, 'catid' => $logoliste));
 				if ((isset($logos) && (!empty($logos)))) {
-				    echo "<hr>\n";
+				    //echo "<hr>\n";
 				    echo $logos;
 				}
 			    }
 			    ?>	
+			    </main>
 		    </div>
 		</div>
 	    	
