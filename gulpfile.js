c4e027bef27015c8e943e2e1679a60cb4dfb2f54
FitoreGashi <fitore.gashi@mondula.com>
2023-06-08 11:18:08 +0200

    #1334 - saved locally cloudflare js

--- gulpfile.js
+++ gulpfile.js
@@ -392,6 +392,40 @@ function makecounterjs() {
 }
 
 
+
+function maked3() {
+    return src([info.source.js + 'cloudflare-scrollmagic/d3.v5.min.js'])
+    .pipe(uglify())
+    .pipe(rename("fau-d3.v5.min.js"))
+    .pipe(dest(info.jsdir))
+    .pipe(touch());
+}
+function scrollmagic() {
+    return src([info.source.js + 'cloudflare-scrollmagic/ScrollMagic.min.js'])
+    .pipe(uglify())
+    .pipe(rename("fau-ScrollMagic.min.js"))
+    .pipe(dest(info.jsdir))
+    .pipe(touch());
+}
+function animationgsap() {
+    return src([info.source.js + 'cloudflare-scrollmagic/animation.gsap.min.js'])
+    .pipe(uglify())
+    .pipe(rename("fau-animation.gsap.min.js"))
+    .pipe(dest(info.jsdir))
+    .pipe(touch());
+}
+
+function debugscroll() {
+    return src([info.source.js + 'cloudflare-scrollmagic/debug.addIndicators.min.js'])
+    .pipe(uglify())
+    .pipe(rename("fau-debug.addIndicators.min.js"))
+    .pipe(dest(info.jsdir))
+    .pipe(touch());
+}
+
+
+
+
 function makesrollstoriesjs() {
     return src([info.source.js + 'main/fau-scroll-stories.js'])
     .pipe(uglify())
@@ -517,7 +551,8 @@ exports.devbuildmainstyle = devbuildmainstyle;
 exports.debugmode = set_debugmode;
 exports.nodebug = unset_debugmode;
 
-var js = series(bundlemainjs, makeslickjs, makecustomblockjs, makecounterjs,makesrollstoriesjs, bundleadminjs, makecustomizerjs, makewplinkjs);
+
+var js = series(bundlemainjs, makeslickjs, makecustomblockjs, makecounterjs,makesrollstoriesjs, bundleadminjs, makecustomizerjs, makewplinkjs, makesrollstoriesjs , debugscroll , scrollmagic, maked3, animationgsap);
 var dev = series(devbuildbackendstyles, devbuildmainstyle, buildprintstyle,  js, devversion);
 
 exports.cssdev = series(devbuildbackendstyles, devbuildmainstyle, buildprintstyle);
